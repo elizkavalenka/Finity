e53ac05d090938d687de9c019b5feeeff2e549d0
elizkavalenka <elizkavalenka@gmail.com>
2021-07-07 01:07:12 +0300

    add 3 section

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 let gulp = require('gulp'),
-    pug = require('gulp-pug');
-    // stylus = require('gulp-stylus'),
+    pug = require('gulp-pug'),
+    stylus = require('gulp-stylus'),
     browserSync = require('browser-sync');
 
-
+    gulp.task('browser-sync', function () {
+        browserSync({
+            server:{
+                baseDir: 'src'
+            },
+            notify: false
+        });
+    });
 
 gulp.task('pug', function () {
     return gulp.src('src/*.pug')
@@ -13,20 +20,19 @@ gulp.task('pug', function () {
         .pipe(gulp.dest('src'))
 });
 
-gulp.task('server', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'src'
-        }
-    });
-    gulp.watch('src/*.html').on("change", browserSync.reload);
+gulp.task('stylus', function(){
+    return gulp.src('src/stylus/style.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('watch', function () {
     gulp.watch('src/*.pug', gulp.parallel('pug'));
+    gulp.watch('src/stylus/main.styl', gulp.parallel('stylus'));
 });
 
 
 
 
-gulp.task('default', gulp.parallel('watch', 'server'));
+gulp.task('default', gulp.parallel('stylus', 'browser-sync', 'watch'));
